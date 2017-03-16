a367531cba63f2ea718d6edea54e767c06201368
mustjoon <mustjoon@gmail.com>
2017-03-16 14:27:21 +0200

    hehehe

--- Admin/src/app/mobile/locations/locations.component.ts
+++ Admin/src/app/mobile/locations/locations.component.ts
@@ -2,7 +2,7 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import {googleMapsService} from '../../secure/map/googleMaps.service';
 import {ActivatedRoute} from "@angular/router";
 import {Router} from '@angular/router';
-
+import {googleMapsServiceFinal} from '../../googleMapsService';
 import { MdlDialogService,
   MdlDialogReference,MdlSnackbarService,IOpenCloseRect } from 'angular2-mdl';
 import {
@@ -25,7 +25,7 @@ import {
   selector: 'locations',
   templateUrl: 'locations.component.html',
   styleUrls: ['locations.component.css'],
-  providers: [googleMapsService]
+  providers: [googleMapsService,googleMapsServiceFinal]
 })
 export class LocationsComponent extends OnInit {
  
@@ -46,7 +46,7 @@ export class LocationsComponent extends OnInit {
 
     router: Router;
 
-    constructor(af: AngularFire, private _routeParams: ActivatedRoute, private googleMapsService : googleMapsService, _router: Router){
+    constructor(af: AngularFire, private _routeParams: ActivatedRoute,private gMapsServ : googleMapsServiceFinal, private googleMapsService : googleMapsService, _router: Router){
       super();
       this.afs = af;
       this.markers = [];
@@ -55,9 +55,8 @@ export class LocationsComponent extends OnInit {
       this._routeParams.params.subscribe(params => {
         this.id = params['id'];
         this.routeName = params['id2'];
-        console.log(this.id);
-        this.locations = af.database.list('/geopark_dev/Reitit/' + this.id + "/" + this.routeName +"/");
-        console.log(this.locations);
+        this.locations = af.database.list('/geopark_dev/Reitit/' + this.id + "/",
+          {query: {orderByKey:true,equalTo:this.routeName}});
       });
       this.user_icon = "../../assets/img/person_icon.png";
       this.marker_icon_locked = "../../assets/img/marker_locked.png";
@@ -68,16 +67,14 @@ export class LocationsComponent extends OnInit {
 
       
     public ngOnInit() { 
-      var mapProp = {
-            center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 7,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+       let gMapsServ = this.gMapsServ;
+       let map = gMapsServ.initMap();
+       this.map = map;
+       let SLPLayer = this.gMapsServ.getSLPlayer(map);
+       this.map.overlayMapTypes.push(SLPLayer);
 
-        this.map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-        let map = this.map;
 
-        this.user_pos = new google.maps.Marker({
+       this.user_pos = new google.maps.Marker({
             position: {lat: 0, lng: 0},
             map: map,
             icon: this.user_icon
@@ -91,8 +88,9 @@ export class LocationsComponent extends OnInit {
 
         let marker_icon_locked = this.marker_icon_locked;
         let marker_icon_tree = this.marker_icon_tree;
-
+        
         setInterval(function(){ 
+
             if (navigator.geolocation) {
               navigator.geolocation.getCurrentPosition(function(position) {
 
@@ -106,109 +104,71 @@ export class LocationsComponent extends OnInit {
                         new google.maps.LatLng(pos), 
                         val.position
                     );
-
-                    if(distance < markerData[index].previewDist[0].$value)
+                  
+                    if(markerData[index].previewDist == 0){
+                        val.setIcon(marker_icon_tree);
+                        val.locked = false;
+                    }
+                    else if(distance < markerData[index].previewDist)
                     {
                         val.setIcon(marker_icon_tree);
+                        val.locked = false;
                     }
                     else
                     {
                         val.setIcon(marker_icon_locked);
+                        val.locked = true;
                     }
+                    
                 });
 
                 var gmPos = new google.maps.LatLng(pos);
                 user_pos.setPosition(gmPos);
               });
             } else {
-              // Browser doesn't support Geolocation
-
                 var infoWindow = new google.maps.InfoWindow({map: map});
-                handleLocationError(false, infoWindow, map.getCenter());
             }
         }, 3000);
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-           
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
 
-            map.setCenter(pos);
-          });
-        } else {
-          // Browser doesn't support Geolocation
-
-            var infoWindow = new google.maps.InfoWindow({map: map});
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-      
-
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-          infoWindow.setPosition(pos);
-          infoWindow.setContent(browserHasGeolocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
-        }
-        
-        let SLPLayer = this.googleMapsService.getSLPlayer(map);
-        this.map.overlayMapTypes.push(SLPLayer);
-
+        this.gMapsServ.centerMapToLocation(map); 
+       
         var afs = this.afs;
-        this.locations.map(function(value){
-          console.log("value",value);
-        })
-
-        let testMarkers = this.markers;
-        let testMarkerData = this.markerData;
-
+       
+        let MarkerData = this.markerData;
         let router = this.router;
-        
+   
         this.locations.subscribe(_dat => {
 
-             var afs = this.afs; 
-
-             var test = _dat.filter(function(value){
-                return value.constructor === Array && value['$key'] == 'Pisteet'; 
-             })
-            
-            test[0].map(function(_point)
+             var afs = this.afs;
+            _dat[0].Pisteet.map(function(_point)
             {
-                let emt = afs.database.list('/geopark_dev/Kohteet/' + _point.category + "/" + _point.id + "/");
+                let kohde = afs.database.list('/geopark_dev/Kohteet/' + _point.category + "/",
+                  {query: {orderByKey:true,equalTo: _point.id}});
 
-                emt.subscribe(_dat => {
-
-                    var long = _dat.filter(function(asd){
-                        return asd.$key == 'longitude';
-                    })
-
-                    var lat = _dat.filter(function(asd){
-                        return asd.$key == 'latitude';
-                    })
-
-                    var previewD = _dat.filter(function(asd){
-                        return asd.$key == 'previewDistance';
-                    })
+                kohde.subscribe(_dat => {
 
                     var marker = new google.maps.Marker({
                         map: map,
-                        position: new google.maps.LatLng(lat[0].$value ,long[0].$value)
+                        position: new google.maps.LatLng(_dat[0].latitude ,_dat[0].longitude)
                     });
+                    marker.locked = true;
+                    if(_dat[0].previewDistance ==0){
+                      marker.locked = false;
+                    }
 
                     marker.addListener('click', function() {
+                      if(!this.locked)
                       router.navigateByUrl('/mobile/kohdepiste/'+_point.category+"/"+_point.id);
                     });
 
                     md.push(marker);
 
-                    var path = {lat :lat[0].$value , lng: long[0].$value};
-                    testMarkers.push(path);
-                    testMarkerData.push({previewDist: previewD});
+                    var path = {lat :_dat[0].latitude , lng: _dat[0].longitude};
+                    markers.push(path);
+                    MarkerData.push({previewDist: _dat[0].previewDistance});
 
                     var flightPath = new google.maps.Polyline({
-                        path: testMarkers,
+                        path: markers,
                         geodesic: true,
                         strokeColor: '#FF0000',
                         strokeOpacity: 1.0,
@@ -217,12 +177,9 @@ export class LocationsComponent extends OnInit {
 
                     flightPath.setMap(map);
                 });
-
-                var markerCluster = new MarkerClusterer(map, testMarkers,
-                {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-                   
+                var markerCluster = new MarkerClusterer(map, markers,
+                {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});  
             });
-           
         });
 
     }
